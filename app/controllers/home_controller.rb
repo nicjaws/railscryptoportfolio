4deffe8b0e0a4708c42f16f2f392a96ef8dd978c
Nicolas Corbalan <outreef@hotmail.com>
2018-05-30 13:37:06 +0000

    Initial Commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,40 @@
+class HomeController < ApplicationController
+  def index
+    require 'net/http'
+    require 'json'
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @coins = JSON.parse(@response)
+    @my_coins = ["BTC", "XRP", "ADA", "XLM", "ETH", "LTC", "BCN", "ICX", "NEO", "STEEM", "IOTA", "XVG", "NANO" ]
+    
+  end
+  
+  def about
+  end
+  
+  def lookup
+    
+    require 'net/http'
+    require 'json'
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    @lookup_coin = JSON.parse(@response)
+
+    
+    @symbol = params[:sym]
+    
+    if @symbol
+       @symbol = @symbol.upcase
+    end
+    
+    if @symbol == ""
+       @symbol = "Hey you forgot to enter a Cryptocurrency"
+    end
+    
+    
+      
+  end
+  
+end
